5dda0bc10141d9e6b1f14ad0e7d88a72167d9d5d
adorsk <adorsk@fas.harvard.edu>
2017-08-09 15:48:35 -0400

    changing to use engine ticks

--- jobman/jobman.py
+++ jobman/jobman.py
@@ -1,12 +1,11 @@
+import collections
 import contextlib
 import json
-import logging
 import os
 from pathlib import Path
 import time
 import traceback
 
-from .job_sources.dir_job_source import DirJobSource
 from . import constants
 from . import debug_utils
 from . import utils
@@ -21,120 +20,69 @@ class JobMan(object):
 
     LOCK_KEY = '_JOBMAN_LOCK'
 
-    CFG_PARAMS = ['dao', 'jobman_db_uri', 'engine', 'max_running_jobs',
-                  'job_engine_states_ttl', 'submission_grace_period',
+    CFG_PARAMS = ['dao', 'jobman_db_uri', 'job_sources', 'engines',
                   'batchable_filters', 'max_batchable_wait',
-                  'target_batch_time', 'default_job_time',
-                  'lock_timeout', 'use_batching']
+                  'target_batch_time', 'default_job_time', 'lock_timeout',
+                  'use_batching']
 
     job_spec_defaults = {'entrypoint': 'entrypoint.sh'}
 
     @classmethod
     def from_cfg(cls, cfg=None):
-        params_from_cfg = {}
-        for param in cls.CFG_PARAMS:
-            try:
-                params_from_cfg[param] = utils.get_key_or_attr(src=cfg,
-                                                               key=param)
-            except KeyError:
-                pass
+        params_from_cfg = utils.get_keys_or_attrs(src=cfg, keys=cls.CFG_PARAMS)
         return JobMan(**params_from_cfg, cfg=cfg)
 
-    def __init__(self, logger=None, logging_cfg=None, debug=None,
-                 setup=True, cfg=None, **kwargs):
+    def __init__(self, logging_cfg=None, debug=None, setup=True, cfg=None,
+                 **kwargs):
         self.debug = debug or os.environ.get('JOBMAN_DEBUG')
         self.logger = self._generate_logger(logging_cfg=logging_cfg)
         self.cfg = cfg
         if setup:
             self._setup(**kwargs)
 
-    def _setup(self, dao=None, jobman_db_uri=None, engine=None,
-               max_running_jobs=50, batchable_filters=...,
-               job_engine_states_ttl=120, submission_grace_period=None,
-               max_batchable_wait=120, target_batch_time=(60 * 60),
-               default_job_time=(5 * 60),
-               lock_timeout=30, use_batching=False, job_source_cfgs=None,
-               job_spec_defaults=None):
+    def _generate_logger(self, logging_cfg=None):
+        logging_cfg = {**(logging_cfg or {})}
+        logging_cfg.setdefault('name', (__name__ + ':' + id(self)))
+        if self.debug:
+            logging_cfg['add_stream_handler'] = True
+            logging_cfg['level'] = 'DEBUG'
+        return utils.generate_logger(logging_cfg)
+
+    def _setup(self, dao=None, jobman_db_uri=None, job_sources=None,
+               engines=None, batchable_filters=None, max_batchable_wait=120,
+               target_batch_time=(60 * 60), default_job_time=(5 * 60),
+               lock_timeout=30, use_batching=False, job_spec_defaults=None):
         self.dao = dao or self._generate_dao(jobman_db_uri=jobman_db_uri)
-        self.engine = engine or self._generate_engine()
-        self.max_running_jobs = max_running_jobs
-        self.job_engine_states_ttl = job_engine_states_ttl
-        self.submission_grace_period = (submission_grace_period or
-                                        (2 * self.job_engine_states_ttl))
+        self.job_sources = job_sources or {}
+        self.engines = engines or {}
         self.default_job_time = default_job_time
         self.lock_timeout = lock_timeout
-        self.submitting_lock_file_name = 'JOBMAN-SUBMITTING'
         self.use_batching = use_batching
-        self.job_sources = self._setup_job_sources(
-            job_source_cfgs=job_source_cfgs)
         self.job_spec_defaults = job_spec_defaults or self.job_spec_defaults
         if batchable_filters is ...:
             batchable_filters = []
-        self.batchable_filters = (batchable_filters
-                                  + self._get_default_batchable_filters())
+        self.batchable_filters = [
+            *(batchable_filters or []),
+            *self._get_default_batchable_filters()
+        ]
         self.max_batchable_wait = max_batchable_wait
         self.target_batch_time = target_batch_time
-
         self.ensure_db()
         self._kvps = {}
 
-    def _generate_logger(self, logging_cfg=None):
-        logging_cfg = logging_cfg or {}
-        logger = (logging_cfg.get('logger') or
-                  logging.getLogger('jobman_%s' % id(self)))
-        log_file = logging_cfg.get('log_file')
-        if log_file:
-            logger.addHandler(logging.FileHandler(Path(log_file).expanduser()))
-        if self.debug:
-            logger.addHandler(logging.StreamHandler())
-        level = logging_cfg.get('level')
-        if level:
-            logger.setLevel(getattr(logging, level))
-        elif self.debug:
-            logger.setLevel(logging.DEBUG)
-        fmt = (logging_cfg.get('fmt') or
-               '| %(asctime)s | %(name)s | %(levelname)s |\n%(message)s\n')
-        formatter = logging.Formatter(fmt)
-        for handler in logger.handlers:
-            handler.setFormatter(formatter)
-        return logger
-
-    def _setup_job_sources(self, job_source_cfgs=None):
-        job_sources = {
-            source_key: self._generate_job_source(
-                source_key=source_key,
-                job_source_cfg=job_source_cfg
-            )
-            for source_key, job_source_cfg in (job_source_cfgs or {}).items()
-        }
-        return job_sources
-
-    def _generate_job_source(self, source_key=None, job_source_cfg=None):
-        source_type = job_source_cfg['type']
-        if source_type == 'dir':
-            SourceCls = DirJobSource
-        else:
-            raise Exception("Unknown job_source type '%s'" % source_type)
-        return SourceCls(jobman=self, key=source_key, **job_source_cfg)
-
     def _debug_locals(self):
-        if self.debug:
-            debug_utils.debug_locals(logger=self.logger)
+        if self.debug: debug_utils.debug_locals(logger=self.logger)  # noqa
+
+    def _generate_dao(self, jobman_db_uri=None):
+        jobman_db_uri = (jobman_db_uri or
+                         str(Path('~/jobman.sqlite.db').expanduser()))
+        from .dao.jobman_sqlite_dao import JobmanSqliteDAO
+        return JobmanSqliteDAO(db_uri=jobman_db_uri, logger=self.logger)
 
     def _get_default_batchable_filters(self):
         def job_spec_has_batchable(job=None):
             return bool(job.get('job_spec', {}).get('batchable'))
         return [job_spec_has_batchable]
-
-    def _generate_dao(self, jobman_db_uri=None):
-        jobman_db_uri = (jobman_db_uri or
-                         str(Path('~/jobman.sqlite.db').expanduser()))
-        from .dao.sqlite_dao import SqliteDAO
-        return SqliteDAO(db_uri=jobman_db_uri, logger=self.logger)
-
-    def _generate_engine(self):
-        from .engines.slurm_engine import SlurmEngine
-        return SlurmEngine(logger=self.logger)
 
     def ensure_db(self):
         self.dao.ensure_db()
@@ -149,47 +97,30 @@ class JobMan(object):
                 self.logger.debug(("_ensure_lock_kvp exc: ", exc))
 
     def submit_job_spec(self, job_spec=None, source_key=None,
-                        source_meta=None, submit_to_engine_immediately=False):
+                        source_meta=None):
         try:
-            job = self._job_spec_to_job(job_spec=job_spec,
-                                        source_key=source_key,
-                                        source_meta=source_meta)
-            created_job = self._create_job(job=job)
-            if submit_to_engine_immediately:
-                created_job = self._submit_job_to_engine(job=created_job)
-            return created_job
+            return self.dao.create_job(job={
+                'job_spec': job_spec,
+                'source_key': source_key,
+                'source_meta': source_meta,
+                'status': 'PENDING',
+            })
         except Exception as exc:
             raise self.SubmissionError() from exc
-
-    def _job_spec_to_job(self, job_spec=None, source_key=None,
-                         source_meta=None):
-        return {
-            'job_spec': job_spec,
-            'source_key': source_key,
-            'source_meta': source_meta,
-            'status': 'PENDING',
-        }
-
-    def _create_job(self, job=None): return self.dao.create_job(job=job)
 
     def get_num_free_slots(self):
         return self.max_running_jobs - len(self.get_running_jobs())
 
-    def get_jobs(self, query=None): return self.dao.get_jobs(query=query)
+    def get_jobs(self, query=None):
+        return self.dao.get_jobs(query=query)
 
     def get_job_for_key(self, key=None):
         return self.dao.get_jobs(query={
             'filters': [{'field': 'key', 'op': '=', 'arg': key}]
         })[0]
 
-    def save_jobs(self, jobs=None): return self.dao.save_jobs(jobs=jobs)
-
-    def get_running_jobs(self, include_batch_subjobs=False):
-        filters = [self.generate_status_filter(status='RUNNING')]
-        if not include_batch_subjobs:
-            filters.append({'field': 'parent_batch_key', 'op': 'IS',
-                            'arg': None})
-        return self.dao.get_jobs(query={'filters': filters})
+    def save_jobs(self, jobs=None):
+        return self.dao.save_jobs(jobs=jobs)
 
     def get_jobs_for_status(self, status=None):
         return self.get_jobs(query={
@@ -218,55 +149,54 @@ class JobMan(object):
         self._kvps[key] = value
 
     def tick(self):
-        if self._job_engine_states_are_stale():
-            self._update_job_engine_states(jobs=self.get_running_jobs())
+        self._tick_engines()
+        self._update_running_jobs()
         self._process_executed_jobs()
         if self.use_batching:
             self._tick_batching()
         self._process_submittable_jobs()
         self._tick_job_sources()
 
-    def _job_engine_states_are_stale(self):
-        job_engine_states_age = self._get_job_engine_states_age()
-        return (
-            (job_engine_states_age is None) or
-            (job_engine_states_age >= self.job_engine_states_ttl)
-        )
+    def _tick_engines(self):
+        for engine in self.engines.values():
+            engine.tick()
 
-    def _get_job_engine_states_age(self):
-        try:
-            age = time.time() - self.get_kvp(key='job_engine_states_modified')
-        except KeyError:
-            age = None
-        return age
+    def _update_running_jobs(self):
+        running_jobs = self.get_running_jobs()
+        self._update_job_engine_states(jobs=running_jobs)
+        self.dao.save_jobs(running_jobs)
+
+    def get_running_jobs(self, include_batch_subjobs=False):
+        filters = [self.generate_status_filter(status='RUNNING')]
+        if not include_batch_subjobs:
+            filters.append({'field': 'parent_batch_key', 'op': 'IS',
+                            'arg': None})
+        return self.dao.get_jobs(query={'filters': filters})
 
     def _update_job_engine_states(self, jobs=None):
-        if not jobs:
-            return
-        keyed_engine_states = self.engine.get_keyed_engine_states(
-            keyed_engine_metas=self._get_keyed_engine_metas(jobs=jobs))
+        if not jobs: return  # noqa
+        jobs_by_engine_key = self._group_jobs_by_engine_key(jobs=jobs)
+        for engine_key, jobs_for_engine_key in jobs_by_engine_key.items():
+            self._update_job_engines_states_for_engine(
+                jobs=jobs_for_engine_key, engine=self.engines[engine_key])
+
+    def _group_jobs_by_engine_key(self, jobs=None):
+        jobs_by_engine = collections.defaultdict(list)
         for job in jobs:
-            self._set_job_engine_state(
-                job=job,
-                job_engine_state=keyed_engine_states.get(job['key'])
-            )
-        self.dao.save_jobs(jobs=jobs)
-        self.set_kvp(key='job_engine_states_modified', value=time.time())
+            jobs_by_engine[job['engine_key']].append(job)
+        return jobs_by_engine
 
-    def _get_keyed_engine_metas(self, jobs=None):
-        jobs = jobs or []
-        keyed_engine_metas = {
-            job['key']: job.get('engine_meta') for job in jobs
-        }
-        return keyed_engine_metas
-
-    def _set_job_engine_state(self, job=None, job_engine_state=None):
-        if job_engine_state is None:
-            if self._job_is_orphaned(job=job):
-                job['status'] = 'EXECUTED'
-        else:
-            job['engine_state'] = job_engine_state
-            job['status'] = job_engine_state.get('status')
+    def _update_job_engine_states_for_engine(self, jobs=None, engine=None):
+        engine_metas = {job['key']: job.get('engine_meta') for job in jobs}
+        engine_states = engine.get_keyed_engine_states(engine_metas)
+        for job in jobs:
+            engine_state = engine_states.get(job['key'])
+            if engine_state is None:
+                if self._job_is_orphaned(job=job):
+                    job['status'] = 'EXECUTED'
+                else:
+                    job['engine_state'] = engine_state
+                    job['status'] = engine_state.get('status')
 
     def _job_is_orphaned(self, job=None):
         return (self._get_job_age(job=job) > self.submission_grace_period)
@@ -397,7 +327,7 @@ class JobMan(object):
             for subjob in subjobs
         ]
         self.save_jobs(jobs=patched_subjobs)
-        self._create_job(job={
+        self.dao.create_job(job={
             'key': batch_key,
             'batch_meta': {'subjob_keys': [job['key'] for job in subjobs]},
             'is_batch': 1,
--- jobman/utils.py
+++ jobman/utils.py
@@ -1,7 +1,43 @@
+from pathlib import Path
+import logging
+
+
 def get_key_or_attr(src=None, key=None, default=...):
-    try: return src[key]
-    except: pass
-    try: return getattr(src, key)
-    except: pass
-    if default is not ...: return default
+    try: return src[key]  # noqa
+    except: pass  # noqa
+    try: return getattr(src, key)  # noqa
+    except: pass  # noqa
+    if default is not ...: return default  # noqa
     raise KeyError(key)
+
+
+def get_keys_or_attrs(src=None, keys=None):
+    result = {}
+    for key in keys:
+        try: result[key] = get_key_or_attr(src=src, key=key)  # noqa
+        except: pass  # noqa
+    return result
+
+
+def generate_logger(logging_cfg=None):
+    logging_cfg = logging_cfg or {}
+    logger = (
+        logging_cfg.get('logger') or
+        logging.getLogger(logging_cfg.get('name', __name__))
+    )
+    log_file = logging_cfg.get('log_file')
+    if log_file:
+        file_handler = logging.FileHandler(Path(log_file).expanduser())
+        logger.addHandler(file_handler)
+    if logging_cfg.get('stream'):
+        logger.addHandler(logging.StreamHandler())
+    level = logging_cfg.get('level')
+    if level:
+        logger.setLevel(getattr(logging, level))
+    formatter = logging.Formatter(
+        logging_cfg.get('format') or
+        '| %(asctime)s | %(name)s | %(levelname)s |\n%(message)s\n'
+    )
+    for handler in logger.handlers:
+        handler.setFormatter(formatter)
+    return logger
